81e836648974de8f74cc5aca11f38596c60d190e
Mario Kudolani <mario.kudolani@gmail.com>
2016-06-03 10:13:44 +0200

    changed structure of HomeView

--- BloodApp.Core/ViewModels/BaseViewModel.cs
+++ BloodApp.Core/ViewModels/BaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MvvmCross.Core.ViewModels;
+﻿using BloodApp.Core.Services;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
 
 namespace BloodApp.Core.ViewModels
 {
@@ -38,5 +40,61 @@ namespace BloodApp.Core.ViewModels
 			base.Start();
 			BaseViewModel.ActiveViewModel = this;
 		}
+
+		#region Navigation Commands
+
+		private IMvxCommand _goToAddDemandCommand;
+		private IMvxCommand _goToAddDonationCommand;
+		private IMvxCommand _logoutCommand;
+
+		public IMvxCommand LogoutCommand
+		{
+			get
+			{
+				if (this._logoutCommand == null) {
+					this._logoutCommand = new MvxCommand(() =>
+					{
+						var userService = Mvx.Resolve<IUserService>();
+						userService.LogoutUser();
+
+						this.Close(this);
+					});
+				}
+
+				return this._logoutCommand;
+			}
+		}
+
+		public IMvxCommand GoToAddDonationCommand
+		{
+			get
+			{
+				if (this._goToAddDonationCommand == null) {
+					this._goToAddDonationCommand = new MvxCommand(() =>
+					{
+						this.ShowViewModel<BloodDonationEditViewModel>();
+					});
+				}
+
+				return this._goToAddDonationCommand;
+			}
+		}
+
+		public IMvxCommand GoToAddDemandCommand
+		{
+			get
+			{
+				if (this._goToAddDemandCommand == null) {
+					this._goToAddDemandCommand = new MvxCommand(() =>
+					{
+						this.ShowViewModel<BloodDemandEditViewModel>();
+					});
+				}
+
+				return this._goToAddDemandCommand;
+			}
+		}
+
+		#endregion
 	}
 }
