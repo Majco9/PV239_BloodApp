a2cf0bb7cca98791a6914b8ba7db1700b3dbf2b6
Mario Kudolani <mario.kudolani@gmail.com>
2016-06-03 15:38:06 +0200

    modified blood type string representation in demand detail, removed blood icon

--- BloodApp.UI.Uwp/Converters/BloodTypeToStringConveter.cs
+++ BloodApp.UI.Uwp/Converters/BloodTypeToStringConveter.cs
@@ -12,24 +12,51 @@ namespace BloodApp.UI.Uwp.Converters
 				return string.Empty;
 			}
 
+			// suffix
+			bool longSuffix = parameter != null && parameter.ToString() == "long";
+
 			BloodType bloodType = (BloodType)value;
 
 			switch (bloodType) {
 				case BloodType.ABNegative:
+					if (longSuffix) {
+						return "AB - neg.";
+					}
 					return "AB-";
 				case BloodType.ABPositive:
+					if (longSuffix) {
+						return "AB - pos.";
+					}
 					return "AB+";
 				case BloodType.ANegative:
+					if (longSuffix) {
+						return "A - neg.";
+					}
 					return "A-";
 				case BloodType.APositive:
+					if (longSuffix) {
+						return "A - pos.";
+					}
 					return "A+";
 				case BloodType.BNegative:
+					if (longSuffix) {
+						return "B - neg.";
+					}
 					return "B-";
 				case BloodType.BPositive:
+					if (longSuffix) {
+						return "B - pos.";
+					}
 					return "B+";
 				case BloodType.ONegative:
+					if (longSuffix) {
+						return "0 - neg.";
+					}
 					return "0-";
 				case BloodType.OPositive:
+					if (longSuffix) {
+						return "0 - pos.";
+					}
 					return "0+";
 
 				default:
